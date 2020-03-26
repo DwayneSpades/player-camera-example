57d6684518d72d761a08287cceb062bc9feb854a
Dwayne_Spades <dwaynespades@gmail.com>
2020-03-26 12:01:11 -0500

    okay remember to include .gitignore

---
+++ 2D Camera Example/Assets/playerController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playerController : MonoBehaviour
+{
+
+    public float lerpTime;
+    public float playerSpeed;
+    public float playerJumpHeight;
+    private float playerDirection;
+
+    public Text playerText;
+    public string playerName;
+    public Camera playerCamera;
+
+
+    void Awake()
+    {
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerText = gameObject.AddComponent<Text>();
+        playerText.text = playerName;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        playerDirection = Input.GetAxis("Horizontal");
+        GetComponent<Rigidbody2D>().AddForce(Vector2.right*playerDirection*playerSpeed);
+
+        if (Input.GetKeyDown("space"))
+        {
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * playerJumpHeight);
+        }
+
+        playerCamera.transform.position =
+            new Vector3(
+                Mathf.Lerp
+                (playerCamera.transform.position.x, transform.position.x, lerpTime),
+                Mathf.Lerp
+                (playerCamera.transform.position.y, transform.position.y, lerpTime), -10);
+    }
+
+    private void LateUpdate()
+    {
+
+        playerText.rectTransform.position = 
+            new Vector2(transform.position.x, transform.position.y);
+
+        
+
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Unity_Player_2DCamera_Example
+ An Example of a smooth 2D player Camera
